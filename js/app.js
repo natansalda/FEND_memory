5e4796aa9f61d7e0861a61820107219fa9662ac2
Natalia Nazaruk <nazarjazz@wp.pl>
2019-02-03 11:47:45 +0100

    Added few comments to structure the project and some starting code

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
- * Create a list that holds all of your cards
+ * A list that holds all of cards in deck
  */
-
+const values = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-anchor', 'fa fa-leaf', 'fa-bicycle', 'fa-diamond', 'fa-bomb', 'fa-leaf', 'fa-bomb', 'fa-bolt', 'fa-bicycle', 'fa-paper-plane-o', 'fa-cube'];
 
 /*
  * Display the cards on the page
@@ -12,7 +12,7 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -20,11 +20,17 @@ function shuffle(array) {
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
+        console.log("YAY!")
     }
-
     return array;
-}
+};
 
+
+/*
+ * shuffle the cards when the user clicks shuffle button
+ */
+ const shuffleButton = document.querySelector('.restart');
+ shuffleButton.addEventListener('click', shuffle(values));
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,3 +42,21 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ const cardClickedListener = document.querySelector('.card').addEventListener('click', cardClicked());
+
+ function cardClicked() {
+ 	console.log("Card has been clicked");
+ };
+
+
+
+// When a user wins the game, a modal appears to congratulate the player and ask if they want to play again. 
+// It should also tell the user how much time it took to win the game, and what the star rating was.
+
+
+// The game displays a star rating (from 1 to at least 3) that reflects the player's performance. 
+// At the beginning of a game, it should display at least 3 stars. After some number of moves, it should change to a lower star rating. 
+// After a few more moves, it should change to a even lower star rating (down to 1). Game displays the current number of moves a user has made.
+
+// When the player starts a game, a displayed timer should also start. Once the player wins the game, the timer stops.
